9e5852c866b0ade2be5e4520465a26f458cd46e3
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-07-07 19:40:19 -0400

    Created script aligining to the index.html code

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Loaded!");
+
+    const form = document.getElementByID("form");
+    const firstname = document.getElementByID("fistname");
+    const lastname = document.getElementByID("lastname");
+
+    form.addEventListener("Submit", function(event)
+
+});
